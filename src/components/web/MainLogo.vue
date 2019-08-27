9a1c4c04557996d6d2c837e668a0c13977679f2c
Christopher Jones <tkcwebdev@gmail.com>
2019-08-26 23:26:27 -0700

    Remaining structure changes

---
+++ src/components/web/MainLogo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main-logo">
+    <img alt="The Next Bill Logo" src="@/assets/logo_light.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainLogo"
+};
+</script>
+
+<style scoped>
+.main-logo img {
+  width: 100px;
+}
+</style>

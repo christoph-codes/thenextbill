d729abfcd173500b9ac053d002f215f74e3e8658
Christopher Jones <thekirkconcept@gmail.com>
2019-10-24 15:59:08 +0000

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,13 +3,14 @@ import firestore from "firebase/firestore";
 
 // Your web app's Firebase configuration
 var config = {
-  apiKey: "AIzaSyDt9dY0WTXXLDyfENj7-Kv2tI2YUbU1xkM",
-  authDomain: "the-next-bill.firebaseapp.com",
-  databaseURL: "https://the-next-bill.firebaseio.com",
-  projectId: "the-next-bill",
-  storageBucket: "",
-  messagingSenderId: "803322993871",
-  appId: "1:803322993871:web:d7614f16a8cb5d3c"
+  apiKey: "AIzaSyDt9dY0WTXXLDyfENj7-Kv2tI2YUbU1xkM",
+  authDomain: "the-next-bill.firebaseapp.com",
+  databaseURL: "https://the-next-bill.firebaseio.com",
+  projectId: "the-next-bill",
+  storageBucket: "the-next-bill.appspot.com",
+  messagingSenderId: "803322993871",
+  appId: "1:803322993871:web:d7614f16a8cb5d3c",
+  measurementId: "G-HK0WQLT6SL"
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(config);
